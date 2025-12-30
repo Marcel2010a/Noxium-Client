a655312009349b3e451b12d272a11b546b5732b8
Marcel2010a <marcelgee151@gmail.com>
2025-12-31 00:20:30 +0100

    Delete noxium-client directory

--- noxium-client/src/client/kotlin/com/antipedroverse/client/APVCClient.kt
+++
@@ -1,9 +0,0 @@
-package com.antipedroverse.client
-
-import net.fabricmc.api.ClientModInitializer
-
-object APVCClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
